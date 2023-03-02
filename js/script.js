ebc831609b43760017e2ee6f53a9b06ddebd8f97
KhaledEl-Sman <69507533+KhaledEl-Sman@users.noreply.github.com>
2023-03-02 18:17:04 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -20,4 +20,18 @@ $(document).ready(function () {
     $("nav a.active").click(function () {
         $(".app-menu").removeClass("opened");
     })
+
+    $("button[type='button']").click(function () {
+        $(this).prev().css('display', 'flex');
+        $("html, body").css("overflow", "hidden");
+    })
 })
+
+$(document).click(function (e) {
+    let container = $(".popup");
+    if (container.is(e.target) && container.has(e.target).length === 0) {
+        $("html, body").css("overflow", "auto");
+        container.css("display", "none");
+        console.log(0);
+    }
+});
